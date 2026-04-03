53d771989f6df1ef16960e7329a0e3c1038bfa40
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-02 20:22:05 -0600

    corregido

--- src/Problema3/Viaje.java
+++ src/Problema3/Viaje.java
@@ -1,5 +1,58 @@
 package Problema3;
 
-public class Viaje {
-}
+import java.time.LocalDate;
+import java.util.List;
 
+public class Viaje {
+
+    //Atributos
+    private String ciudad;
+    private String destino;
+    private String direccion;
+    private LocalDate fechaDeViaje;
+    private LocalDate fechaDeRegreso;
+    private String descripcionDeCarga;
+    private float montoDelViaje;
+
+    //Constructor
+    public Viaje(String ciudad, String destino,String direccion,LocalDate fechaDeViaje,
+                 LocalDate fechaDeRegreso,String descripcionDeCarga, float montoDelViaje) {
+        this.ciudad=ciudad;
+        this.destino=destino;
+        this.direccion=direccion;
+        this.fechaDeViaje=fechaDeViaje;
+        this.fechaDeRegreso=fechaDeRegreso;
+        this.descripcionDeCarga=descripcionDeCarga;
+        this.montoDelViaje=montoDelViaje;
+    }
+
+    //Metodos
+    public String getCiudad() {
+        return ciudad;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public String getDestino() {
+        return destino;
+    }
+    public String getDescripcionDeCarga() {
+        return descripcionDeCarga;
+    }
+    public LocalDate getFechaDeViaje() {
+        return fechaDeViaje;
+    }
+    public LocalDate getFechaDeRegreso() {
+        return fechaDeRegreso;
+    }
+    public float getMontoDelViaje() {
+        return montoDelViaje;
+    }
+
+    @Override
+    public String toString() {
+        return "Viaje a:"+ciudad+"\n"+
+                "Destino:"+destino+"\n"+"Carga: "+descripcionDeCarga+"\n"+
+                "Monto del viaje:"+montoDelViaje;
+    }
+}

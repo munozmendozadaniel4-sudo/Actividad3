e3531f6570876839bb3bfd08512274d0de25a818
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-02 20:20:47 -0600

    corregido

--- src/Problema2/Receta.java
+++ src/Problema2/Receta.java
@@ -1,5 +1,42 @@
 package Problema2;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Receta {
+
+    //Atributos
+    private List<String> listaDeMedicamentos;
+    private List<String> listaDeRecomendaciones;
+
+    //Constructor
+    public Receta() {
+        this.listaDeMedicamentos = new ArrayList<>();
+        this.listaDeRecomendaciones = new ArrayList<>();
+    }
+
+    //metodos
+    public void agregarMedicamento(String medicamento){
+        listaDeMedicamentos.add(medicamento);
+    }
+    public void agregarRecomendacion(String recomendacion){
+        listaDeRecomendaciones.add(recomendacion);
+    }
+
+    public List<String> getListaDeMedicamentos() {
+        return listaDeMedicamentos;
+    }
+
+    public List<String> getListaDeRecomendaciones() {
+        return listaDeRecomendaciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Receta Medica"+"\n"+"Medicamentos Recetados:"+listaDeMedicamentos+"\n"+
+                "Recomendaciones"+listaDeRecomendaciones;
+    }
 }
+
 

7e072a3300190359485b40becc46f2f2cb1bea77
Daniel Muñoz Mendoza <munozmendozadaniel4@gmail.com>
2026-04-02 20:21:08 -0600

    corregido

--- src/Problema2/Sintoma.java
+++ src/Problema2/Sintoma.java
@@ -1,5 +1,37 @@
 package Problema2;
 
 public class Sintoma {
-}
+    //atributos
+    private String descripcion;
+    private float temperatura;
+    private float peso;
+    private float altura;
 
+    //constructor
+    public Sintoma(String descripcion, float peso, float altura, float temperatura) {
+        this.descripcion = descripcion;
+        this.temperatura = temperatura;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    //Metodos
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public float getAltura() {
+        return altura;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public float getTemperatura() {
+        return temperatura;
+    }
+
+    @Override
+    public String toString() {
+        return "Sintoma del paciente: "+descripcion+"\n"+"Temperatura: "+temperatura+"\n"+
+                "Peso: "+peso+"\n"+"Altura: "+"\n";
+    }
+}
